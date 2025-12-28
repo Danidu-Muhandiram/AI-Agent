fc8aef5a8165c040e4e07becf34af81a398a725c
Danidu Muhandiram <muhandiramdanidu@gmail.com>
2025-12-28 20:56:45 +0530

    import the core frameworks, code basic things in main function

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+# langchain is a high-level framework for building applications powered by language models.
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+# langgraph is a framework for building AI agents using a graph-based approach.
+from langgraph.prebuilt import create_react_agent
+# dotenv is used to load environment variables from a .env file.
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 def main():
-    print("Hello from python-ai-agent!")
+    model = ChatOpenAI(temperature=0)
 
+    tools = []
+    agent_executor = create_react_agent(model, tools=tools)
 
-if __name__ == "__main__":
-    main()
+    print("Welcome. I am your AI Agent. Type 'exit' to quit.")
+    print("You can ask questions or chat with me")
+
+    while True:
